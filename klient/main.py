317471eb828600791e125dd8b9c2648a13b509c7
Mats Ola Sand <matsolasand@outlook.com>
2026-01-11 16:27:39 +0100

    [DEV] main follows the script of main_flere_ferdigheter

--- klient/main.py
+++ klient/main.py
@@ -7,41 +7,41 @@ def hent_konsulentregister():
     data = response.json()
     return data
 
-# ----------------------------------------------------------------------
+# ------------------------------------------------------------------
 
 def filtrer_konsulenter(
     konsulenter : list[dict],
     min_tilgjengelighet_prosent : int,
-    pakrevd_ferdighet : str
+    pakrevde_ferdigheter : list[str]
 ) -> list[dict]:
     tilgjengelige_konsulenter = []
     for konsulent in konsulenter:
-        ferdigheter = konsulent["ferdigheter"]
         tilgjengelighet = 100 - konsulent["belastning_prosent"]
-        if (
-            pakrevd_ferdighet in ferdigheter
-        ) and (
-            tilgjengelighet >= min_tilgjengelighet_prosent
-        ):
-            tilgjengelig = {
+        har_ferdigheter = all(
+            ferdighet in konsulent["ferdigheter"]
+            for ferdighet in pakrevde_ferdigheter
+        )
+        if har_ferdigheter and tilgjengelighet >= min_tilgjengelighet_prosent:
+            tilgjengelige_konsulenter.append({
                 "navn" : konsulent["navn"],
                 "tilgjengelighet" : tilgjengelighet
-            }
-            tilgjengelige_konsulenter.append(tilgjengelig)
+            })
     return tilgjengelige_konsulenter
 
-# ----------------------------------------------------------------------
+# ------------------------------------------------------------------
 
 def lag_sammendrag(
     tilgjengelige_konsulenter : list[dict],
     min_tilgjengelige_prosent : int,
-    pakrevd_ferdighet : str
+    pakrevde_ferdigheter : list[str]
 ) -> list[dict]:
-    sammendrag = [
-        f"Fant {len(tilgjengelige_konsulenter)} konsulenter med minst "
-        + f"{min_tilgjengelige_prosent}% tilgjengelighet og ferdigheten "
-        + f"'{pakrevd_ferdighet}'."
-    ]
+    sammendrag = []
+    strengkonvensjon(
+        sammendrag,
+        tilgjengelige_konsulenter,
+        min_tilgjengelige_prosent,
+        pakrevde_ferdigheter
+    )
     for konsulent in tilgjengelige_konsulenter:
         navn = konsulent["navn"]
         tilgjengelighet = konsulent["tilgjengelighet"]
@@ -52,34 +52,67 @@ def lag_sammendrag(
     respons = {
         "sammendrag" : " ".join(sammendrag)
     }
+    respons["sammendrag"] = respons["sammendrag"].replace(" .", ".")
     return respons
 
-# ----------------------------------------------------------------------
+# ------------------------------------------------------------------
+
+def strengkonvensjon(
+    sammendrag : list[str],
+    tilgjengelige_konsulenter : list[dict],
+    min_tilgjengelige_prosent : int,
+    pakrevde_ferdigheter : list[str]
+) -> None:
+    if len(tilgjengelige_konsulenter) == 1:
+        sammendrag.append(
+            f"Fant {len(tilgjengelige_konsulenter)} konsulent med minst"
+        )
+    else:
+        sammendrag.append(
+            f"Fant {len(tilgjengelige_konsulenter)} konsulenter med minst"
+        )
+    if len(pakrevde_ferdigheter) == 1:
+        sammendrag.append(
+            f"{min_tilgjengelige_prosent}% tilgjengelighet og ferdigheten "
+            + f"'{pakrevde_ferdigheter[0]}'."
+        )
+        return
+    sammendrag.append(
+        f"{min_tilgjengelige_prosent}% tilgjengelighet og ferdighetene"
+    )
+    for i in range(len(pakrevde_ferdigheter)):
+        sammendrag.append(f"'{pakrevde_ferdigheter[i]}'")
+        if (i + 1) < len(pakrevde_ferdigheter):
+            sammendrag.append("og")
+    sammendrag.append(".")
+
+# ------------------------------------------------------------------
 
 app = FastAPI()
 
 @app.get("/tilgjengelige-konsulenter/sammendrag")
 def sammendrag_egnede_konsulenter(
     min_tilgjengelighet_prosent : int = Query(..., ge=50, le=100),
-    pakrevd_ferdighet : str = Query(...)
+    pakrevde_ferdigheter : list[str] = Query(...)
 ):
     konsulentregister = hent_konsulentregister()
     tilgjengelige_konsulenter = filtrer_konsulenter(
         konsulenter = konsulentregister,
         min_tilgjengelighet_prosent = min_tilgjengelighet_prosent,
-        pakrevd_ferdighet = pakrevd_ferdighet
+        pakrevde_ferdigheter = pakrevde_ferdigheter
     )
     respons = lag_sammendrag(
         tilgjengelige_konsulenter = tilgjengelige_konsulenter,
         min_tilgjengelige_prosent = min_tilgjengelighet_prosent,
-        pakrevd_ferdighet = pakrevd_ferdighet
+        pakrevde_ferdigheter = pakrevde_ferdigheter
     )
     return respons
 
 
 def main():
     respons = sammendrag_egnede_konsulenter(
-        min_tilgjengelighet_prosent = 50, pakrevd_ferdighet = "python"
+        min_tilgjengelighet_prosent = 50,
+        pakrevde_ferdigheter = ["python", "c++"]
     )
     print(respons)
 
